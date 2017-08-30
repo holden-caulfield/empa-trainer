02e0ef26b534f0ea21e594c38e0dd193f2a1da80
JP Saraceno <jsarac@gmail.com>
2017-08-30 14:58:16 -0300

    show interval info

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react'
 import Soundfont from 'soundfont-player'
+import { intervallic } from 'tonal-harmonizer'
 import logo from './logo.svg'
 import { randomInterval } from './lib/music'
 import './App.css'
 
 const ac = new AudioContext()
 
-const playInterval = () => {
-  Soundfont.instrument(ac, 'acoustic_grand_piano')
-  .then((piano) => {
-    randomInterval().forEach(
-      (note, index) => piano.play(note, ac.currentTime + index, {duration: 1})       
-    )
-  })
-}
+const renderInterval = (interval) =>
+  <div>
+    <p>Root Note: {interval[0]}</p>
+    <p>Name: {intervallic(interval)}</p>
+    <p>Note: {interval[1]}</p>
+  </div>
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      interval: null
+    };
+  }
+
+  playInterval = () => {
+    Soundfont.instrument(ac, 'acoustic_grand_piano')
+    .then((piano) => {
+      const interval = randomInterval()
+      interval.forEach(
+        (note, index) => {
+          piano.play(note, ac.currentTime + index, {duration: 1})
+        }       
+      )
+      this.setState({interval})      
+    })
+  }  
+
   render() {
+    const { interval } = this.state
     return (
       <div className="App">
         <div className="App-header">
@@ -26,7 +46,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button onClick={playInterval}>Give me an 3rd interval!</button>
+        <button onClick={this.playInterval}>Give me a 3rd interval!</button>
+        { interval && renderInterval(interval)}
       </div>
     )
   }
