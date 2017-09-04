6f0bd6384f8caa2fc7334648af7ff989a22d8fb4
JP Saraceno <jsarac@gmail.com>
2017-09-04 16:14:19 -0300

    add redux-observable, add mock for web audio on tests

--- src/Redux/Intervals/index.js
+++ src/Redux/Intervals/index.js
@@ -1,18 +1,22 @@
 import { createReducer, createActions } from 'reduxsauce'
 import { randomInterval } from '../../lib/music'
 import { playInterval } from '../../lib/player'
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/mapTo';
 
 /* ------------- Initial State ------------- */
 
 const INITIAL_STATE = {
     interval: null,
-    answer: null
+    answer: null,
+    ready: false
 }
 
 /* ------------- Types and Action Creators ------------- */
 
 const { Types, Creators } = createActions({
     start: null,
+    ready: null,
     answer: ['answer']
 })
   
@@ -20,19 +24,29 @@ export default Creators
   
 /* ------------- Reducers ------------- */
   
-const start = (state) => {
-  const interval = randomInterval()
-  playInterval(interval)
-  return ({ interval, answer: null })
-}
+const start = (state) => 
+  ({ interval: randomInterval(), answer: null, ready: false })
 
 const answer = (state, { answer }) =>
   ({ ...state, answer })
+
+const ready = (state) =>
+  ({ ...state, ready: true})
+
 /* ------------- Hookup Reducers To Types ------------- */
   
 export const reducer = createReducer(INITIAL_STATE, {
   [Types.START]: start,
-  [Types.ANSWER]: answer
+  [Types.ANSWER]: answer,
+  [Types.READY]: ready
 })
   
+/* ------------- Epics ------------- */
+
+export const epic = (action$, store) =>
+action$.ofType(Types.START)
+  .do(() => {playInterval(store.getState().intervals.interval)})
+  .mapTo(Creators.ready())
+
+
 /* ------------- Selectors ------------- */
--- src/Redux/index.js
+++ src/Redux/index.js
@@ -1,8 +1,21 @@
-import { createStore, combineReducers } from 'redux'
-import { reducer as intervals } from './Intervals'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { combineEpics, createEpicMiddleware } from 'redux-observable'
+
+import { reducer as intervals, epic as intervalsEpic } from './Intervals'
+
+const rootReducer = combineReducers({
+  intervals
+})
+
+const rootEpic = combineEpics(
+  intervalsEpic
+)
+
+const middlewares = [
+  createEpicMiddleware(rootEpic)
+]
 
 export default () => createStore(
-  combineReducers({
-    intervals
-  })
+  rootReducer,
+  applyMiddleware(...middlewares)
 )
