c91df5a5ae5749d05391fc5ddca895d244abaace
JP Saraceno <jp@scvsoft.com>
2019-11-14 23:41:28 -0300

    Updates

---
+++ precache-manifest.8d889a8b80bb6428bbfd7dca18e386e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05221f80d693c091af7e13299fa2e0ea",
+    "url": "/empa-trainer/index.html"
+  },
+  {
+    "revision": "bef831bffd4f57ca26cd",
+    "url": "/empa-trainer/static/css/main.5b8c8a51.chunk.css"
+  },
+  {
+    "revision": "87e38fbc59c2279a927c",
+    "url": "/empa-trainer/static/js/2.40cdf1c3.chunk.js"
+  },
+  {
+    "revision": "bef831bffd4f57ca26cd",
+    "url": "/empa-trainer/static/js/main.05b401c6.chunk.js"
+  },
+  {
+    "revision": "879880cda8e4b35f2d88",
+    "url": "/empa-trainer/static/js/runtime-main.c17c90d1.js"
+  }
+]);
